4e92c23e886b5c4952080abb75d6c1e6ad6e025a
Gabriele Viglianisi <vigliag@gmail.com>
2017-05-10 12:40:08 +0200

    working fndetectcrypto and andded memsavep

--- panda/plugins/fndetectcrypto/fndetectcrypto.cpp
+++ panda/plugins/fndetectcrypto/fndetectcrypto.cpp
@@ -36,6 +36,7 @@ using namespace std;
 using fnid = target_ulong;
 
 target_ulong tracked_asid = 0;
+target_ulong current_asid = 0;
 
 //NOTE we'll assume a BB is contained into a function
 //TODO should I handle BB invalidation?
@@ -62,9 +63,9 @@ public:
     }
 };
 
-void countAndAddToCache(TranslationBlock *tb){
+InstCount countAndAddToCache(TranslationBlock *tb){
     FnInstVisitor FIV;
-    llvm::Function* bbfn = (llvm::Function*) tb->llvm_tc_ptr;
+    llvm::Function* bbfn = tb->llvm_function;
    
     if(bbfn){
         FIV.visit(*bbfn);
@@ -74,57 +75,67 @@ void countAndAddToCache(TranslationBlock *tb){
 
     target_ulong tb_identifier = tb->pc;
     bb_count_cache[tb_identifier] = FIV.count;
+    return FIV.count;
 }
 
+// invalidate cache when translating/re-translating
 int after_block_translate(CPUState *cpu, TranslationBlock *tb) {
-    CPUArchState* env = (CPUArchState*)cpu->env_ptr;
+    bb_count_cache.erase(tb->pc);
+    return 0;
+}
 
-    if (panda_in_kernel(cpu) ||
-         tracked_asid != panda_current_asid(ENV_GET_CPU(env))){
-        return 0;
-    }
-    
-    if(!llvm_enabled){
+// ASSUMING cr3 is changed inside of a block executing kernel code,
+// then jumps inside of application code (which hasn't been translated yet,
+// if this is the first time).
+int asid_changed(CPUState *env, target_ulong old_asid, target_ulong new_asid) {      
+    current_asid = new_asid;
+
+    if(current_asid == tracked_asid && !llvm_enabled){
         printf("enabling llvm\n");
         panda_enable_llvm();
         panda_enable_llvm_helpers();
         llvm_enabled = true;
     }
 
-    countAndAddToCache(tb);
-
     return 0;
 }
 
-int before_block_exec_cb(CPUState *cpu, TranslationBlock *tb){
-    CPUArchState *env = (CPUArchState*)cpu->env_ptr;
-
-    // only go on if the asid is the one we are tracking
-    if (panda_in_kernel(cpu) ||
-         tracked_asid != panda_current_asid(ENV_GET_CPU(env))){
-        return 0;
-    }
-    
-    // get the current function identifier
+fnid getCurrentFunction(CPUState *cpu){
     target_ulong fns[1];
     int fns_returned = get_functions(fns,1,cpu);
     if(!fns_returned){
         printf("No fns returned\n");
         return 0;
     }
-    fnid current_fn = fns[0];
+    return fns[0];
+}
 
-    // get the identifier of this basic block
+// Before executing a basic block, check if the cr3 is the right one, if it is,
+// take current executing function, and assign it the instruction count of the block
+// ASSUMING calls are executed in the previous block
+int before_block_exec_cb(CPUState *cpu, TranslationBlock *tb){
+    //CPUArchState *env = (CPUArchState*)cpu->env_ptr;
+
+    // only go on if the asid is the one we are tracking
+    if (panda_in_kernel(cpu) ||
+        tracked_asid != current_asid){
+        return 0;
+    }
+    
+    fnid current_fn = getCurrentFunction(cpu);
     target_ulong tb_identifier = tb->pc;
     
+    // Count instructions in this basic block 
+    // (take from cache if already counted)
+    InstCount bbcount; 
     if (bb_count_cache.count(tb_identifier)) {
-        InstCount& bbcount = bb_count_cache[tb_identifier];
-        
-        totalCount[current_fn].arit += bbcount.arit;
-        totalCount[current_fn].tot += bbcount.tot;
+        bbcount = bb_count_cache[tb_identifier];
     } else {
-        printf("bb " TARGET_FMT_ld " not found in cache, this shouldn't happen \n", tb->pc);
+        bbcount = countAndAddToCache(tb);
     }
+
+    totalCount[current_fn].arit += bbcount.arit;
+    totalCount[current_fn].tot += bbcount.tot;
 
     return 0;
 }
@@ -143,6 +154,11 @@ bool init_plugin(void *self) {
     panda_register_callback(self, PANDA_CB_AFTER_BLOCK_TRANSLATE, pcb);
     pcb.before_block_exec = before_block_exec_cb;
     panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, pcb);
+    pcb.asid_changed = asid_changed;
+    panda_register_callback(self, PANDA_CB_ASID_CHANGED, pcb);
+
+    //TODO actually needed?
+    panda_disable_tb_chaining();
 
     panda_arg_list *args = panda_get_args("fndetectcrypto");
     if (args != NULL) {
@@ -150,11 +166,6 @@ bool init_plugin(void *self) {
     }
 
     printf("tracking asid " TARGET_FMT_ld  " \n", tracked_asid);
-
-    printf("enabling llvm\n");
-    panda_enable_llvm();
-    panda_enable_llvm_helpers();
-    llvm_enabled = true;
 
     return true;
 }
